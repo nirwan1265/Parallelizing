e4b737399230a8a75a8bf36bee05a0b78d294c33
nirwan1265 <88001815+nirwan1265@users.noreply.github.com>
2022-04-12 14:47:36 -0400

    parallel

---
+++ parallelizing.R
@@ -0,0 +1,25 @@
+#automatic install of packages if they are not installed already
+list.of.packages <- c(
+  "foreach",
+  "doParallel",
+  "ranger",
+  "palmerpenguins",
+  "tidyverse",
+  "kableExtra"
+)
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if(length(new.packages) > 0){
+  install.packages(new.packages, dep=TRUE)
+}
+
+#loading packages
+for(package.i in list.of.packages){
+  suppressPackageStartupMessages(
+    library(
+      package.i, 
+      character.only = TRUE
+    )
+  )
+}
